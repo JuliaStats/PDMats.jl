4038d66e2a78dca0f8e9b7819a1f98e3401b7951
John Pearson <jmxpearson@gmail.com>
2016-03-23 14:33:24 -0400

    generalize pdmats for automatic differentiation

--- src/pdiagmat.jl
+++ src/pdiagmat.jl
@@ -1,6 +1,6 @@
 # positive diagonal matrix
 
-immutable PDiagMat{T<:AbstractFloat,V<:AbstractVector} <: AbstractPDMat{T}
+immutable PDiagMat{T<:Real,V<:AbstractVector} <: AbstractPDMat{T}
   dim::Int
   diag::V
   inv_diag::V
@@ -24,7 +24,7 @@ diag(a::PDiagMat) = copy(a.diag)
 
 ### Arithmetics
 
-function pdadd!{T<:AbstractFloat}(r::Matrix{T}, a::Matrix{T}, b::PDiagMat{T}, c::T)
+function pdadd!(r::Matrix, a::Matrix, b::PDiagMat, c)
     @check_argdims size(r) == size(a) == size(b)
     if is(r, a)
         _adddiag!(r, b.diag, c)
@@ -34,9 +34,9 @@ function pdadd!{T<:AbstractFloat}(r::Matrix{T}, a::Matrix{T}, b::PDiagMat{T}, c:
     return r
 end
 
-*{T<:AbstractFloat}(a::PDiagMat{T}, c::T) = PDiagMat(a.diag * c)
-*{T<:AbstractFloat}(a::PDiagMat{T}, x::StridedVecOrMat{T}) = a.diag .* x
-\{T<:AbstractFloat}(a::PDiagMat{T}, x::StridedVecOrMat{T}) = a.inv_diag .* x
+*{T<:Real}(a::PDiagMat, c::T) = PDiagMat(a.diag * c)
+*(a::PDiagMat, x::StridedVecOrMat) = a.diag .* x
+\(a::PDiagMat, x::StridedVecOrMat) = a.inv_diag .* x
 
 
 ### Algebra
@@ -49,7 +49,7 @@ eigmin(a::PDiagMat) = minimum(a.diag)
 
 ### whiten and unwhiten
 
-function whiten!{T<:AbstractFloat}(r::StridedVector{T}, a::PDiagMat{T}, x::StridedVector{T})
+function whiten!(r::StridedVector, a::PDiagMat, x::StridedVector)
     n = dim(a)
     @check_argdims length(r) == length(x) == n
     v = a.inv_diag
@@ -59,7 +59,7 @@ function whiten!{T<:AbstractFloat}(r::StridedVector{T}, a::PDiagMat{T}, x::Strid
     return r
 end
 
-function unwhiten!{T<:AbstractFloat}(r::StridedVector{T}, a::PDiagMat{T}, x::StridedVector{T})
+function unwhiten!(r::StridedVector, a::PDiagMat, x::StridedVector)
     n = dim(a)
     @check_argdims length(r) == length(x) == n
     v = a.diag
@@ -69,40 +69,40 @@ function unwhiten!{T<:AbstractFloat}(r::StridedVector{T}, a::PDiagMat{T}, x::Str
     return r
 end
 
-whiten!{T<:AbstractFloat}(r::StridedMatrix{T}, a::PDiagMat{T}, x::StridedMatrix{T}) =
+whiten!(r::StridedMatrix, a::PDiagMat, x::StridedMatrix) =
     broadcast!(*, r, x, sqrt(a.inv_diag))
 
-unwhiten!{T<:AbstractFloat}(r::StridedMatrix{T}, a::PDiagMat{T}, x::StridedMatrix{T}) =
+unwhiten!(r::StridedMatrix, a::PDiagMat, x::StridedMatrix) =
     broadcast!(*, r, x, sqrt(a.diag))
 
 
 ### quadratic forms
 
-quad{T<:AbstractFloat}(a::PDiagMat{T}, x::Vector{T}) = wsumsq(a.diag, x)
-invquad{T<:AbstractFloat}(a::PDiagMat{T}, x::Vector{T}) = wsumsq(a.inv_diag, x)
+quad(a::PDiagMat, x::Vector) = wsumsq(a.diag, x)
+invquad(a::PDiagMat, x::Vector) = wsumsq(a.inv_diag, x)
 
-quad!{T<:AbstractFloat}(r::AbstractArray{T}, a::PDiagMat{T}, x::Matrix{T}) = At_mul_B!(r, abs2(x), a.diag)
-invquad!{T<:AbstractFloat}(r::AbstractArray{T}, a::PDiagMat{T}, x::Matrix{T}) = At_mul_B!(r, abs2(x), a.inv_diag)
+quad!(r::AbstractArray, a::PDiagMat, x::Matrix) = At_mul_B!(r, abs2(x), a.diag)
+invquad!(r::AbstractArray, a::PDiagMat, x::Matrix) = At_mul_B!(r, abs2(x), a.inv_diag)
 
 
 ### tri products
 
-function X_A_Xt{T<:AbstractFloat}(a::PDiagMat{T}, x::StridedMatrix{T})
+function X_A_Xt(a::PDiagMat, x::StridedMatrix)
     z = x .* reshape(sqrt(a.diag), 1, dim(a))
     A_mul_Bt(z, z)
 end
 
-function Xt_A_X{T<:AbstractFloat}(a::PDiagMat{T}, x::StridedMatrix{T})
+function Xt_A_X(a::PDiagMat, x::StridedMatrix)
     z = x .* sqrt(a.diag)
     At_mul_B(z, z)
 end
 
-function X_invA_Xt{T<:AbstractFloat}(a::PDiagMat{T}, x::StridedMatrix{T})
+function X_invA_Xt(a::PDiagMat, x::StridedMatrix)
     z = x .* reshape(sqrt(a.inv_diag), 1, dim(a))
     A_mul_Bt(z, z)
 end
 
-function Xt_invA_X{T<:AbstractFloat}(a::PDiagMat{T}, x::StridedMatrix{T})
+function Xt_invA_X(a::PDiagMat, x::StridedMatrix)
     z = x .* sqrt(a.inv_diag)
     At_mul_B(z, z)
 end
