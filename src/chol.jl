4b76569a2c8d9701253311a482b078074e8df0b2
Seth Axen <seth.axen@gmail.com>
2021-06-11 11:05:36 +0200

    Add utilities for getting stored factor

--- src/chol.jl
+++ src/chol.jl
@@ -1,6 +1,10 @@
 CholType{T,S<:AbstractMatrix} = Cholesky{T,S}
 chol_lower(a::Matrix) = cholesky(a).L
 
+# always use the stored cholesky factor, not a copy
+@inline chol_lower(a::CholType) = a.uplo === 'L' ? a.L : transpose(a.U)
+@inline chol_upper(a::CholType) = a.uplo === 'U' ? a.U : transpose(a.L)
+
 if HAVE_CHOLMOD
     CholTypeSparse{T} = SuiteSparse.CHOLMOD.Factor{T}
 
