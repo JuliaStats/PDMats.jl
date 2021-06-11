3f37f864f6fe50d8692b1be28a7052bc4ffd17ac
Seth Axen <seth.axen@gmail.com>
2021-06-11 12:12:09 +0200

    Apply suggestions from code review
    Co-authored-by: Andreas Noack <andreas@noack.dk>

--- src/chol.jl
+++ src/chol.jl
@@ -2,8 +2,8 @@ CholType{T,S<:AbstractMatrix} = Cholesky{T,S}
 chol_lower(a::Matrix) = cholesky(a).L
 
 # always use the stored cholesky factor, not a copy
-@inline chol_lower(a::CholType) = a.uplo === 'L' ? a.L : transpose(a.U)
-@inline chol_upper(a::CholType) = a.uplo === 'U' ? a.U : transpose(a.L)
+chol_lower(a::CholType) = a.uplo === 'L' ? a.L : a.U'
+@inline chol_upper(a::CholType) = a.uplo === 'U' ? a.U : a.L'
 
 if HAVE_CHOLMOD
     CholTypeSparse{T} = SuiteSparse.CHOLMOD.Factor{T}
