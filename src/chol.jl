9804a606d4dc2ee321a73c42d64ca940f414abc8
ST John <st--@users.noreply.github.com>
2021-11-03 17:03:45 +0200

    avoid copy in chol_lower(::Matrix)

--- src/chol.jl
+++ src/chol.jl
@@ -1,9 +1,10 @@
 CholType{T,S<:AbstractMatrix} = Cholesky{T,S}
-chol_lower(a::Matrix) = cholesky(a).L
 
 # always use the stored cholesky factor, not a copy
 chol_lower(a::CholType) = a.uplo === 'L' ? a.L : a.U'
 chol_upper(a::CholType) = a.uplo === 'U' ? a.U : a.L'
+
+chol_lower(a::Matrix) = chol_lower(cholesky(a))
 
 if HAVE_CHOLMOD
     CholTypeSparse{T} = SuiteSparse.CHOLMOD.Factor{T}
