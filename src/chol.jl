84df00b41da9573c3e0b7e79bdcde2dda24bb2d5
David Widmann <devmotion@users.noreply.github.com>
2022-04-27 16:31:00 +0200

    Improve  `chol_lower` and `chol_upper` (#149)
    * Improve  `chol_lower` and `chol_upper`
    
    * Increase allowed overhead for Ubuntu x64 on Julia 1.0
    
    Co-authored-by: Andreas Noack <andreas@noack.dk>

--- src/chol.jl
+++ src/chol.jl
@@ -1,7 +1,14 @@
 # Accessing a.L directly might involve an extra copy();
-# instead, always use the stored Cholesky factor:
-chol_lower(a::Cholesky) = a.uplo === 'L' ? a.L : a.U'
-chol_upper(a::Cholesky) = a.uplo === 'U' ? a.U : a.L'
+# instead, always use the stored Cholesky factor
+# Using `a.factors` instead of `a.L` or `a.U` avoids one
+# additional `LowerTriangular` or `UpperTriangular` wrapper and
+# leads to better performance
+function chol_lower(a::Cholesky)
+    return a.uplo === 'L' ? LowerTriangular(a.factors) : LowerTriangular(a.factors')
+end
+function chol_upper(a::Cholesky)
+    return a.uplo === 'U' ? UpperTriangular(a.factors) : UpperTriangular(a.factors')
+end
 
 # For a dense Matrix, the following allows us to avoid the Adjoint wrapper:
 chol_lower(a::Matrix) = cholesky(Symmetric(a, :L)).L
