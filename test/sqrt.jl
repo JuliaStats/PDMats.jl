0281deb54273d456a328ce0ac9a69adcde9deb41
William Wilkinson <william.wilkinson@invenialabs.co.uk>
2022-04-28 17:53:12 +0100

    no longer require specific sparse cholesky test since sqrt(PDSparse) is a PDMat

--- test/sqrt.jl
+++ test/sqrt.jl
@@ -10,18 +10,6 @@ function _pd_sqrt_compare(A::AbstractPDMat)
     _pd_compare(PDAsqrt, PDAaqrt_dense)
 end
 
-function _pdsparse_sqrt_compare(A::AbstractPDMat)
-    # specific method required for testing cholesky of a sparse matrix
-    Asqrt = sqrt(A)
-    Asqrt_dense = PDMat(sqrt(Matrix(A)))
-    @test dim(Asqrt) == dim(Asqrt_dense)
-    @test Matrix(Asqrt) ≈ Matrix(Asqrt_dense)
-    cAsqrt = cholesky(Asqrt)
-    perm = cAsqrt.p  # account for permutation
-    L = sparse(cAsqrt.L)
-    @test Asqrt[perm, perm] ≈ L * L'
-end
-
 n = 10
 
 @testset "Matrix square root" begin
@@ -29,6 +17,6 @@ n = 10
         _pd_sqrt_compare( _randPDMat(T, n))
         _pd_sqrt_compare( _randPDiagMat(T, n))
         _pd_sqrt_compare( _randScalMat(T, n))
-        _pdsparse_sqrt_compare( _randPDSparseMat(T, n))
+        _pd_sqrt_compare( _randPDSparseMat(T, n))
     end
 end
