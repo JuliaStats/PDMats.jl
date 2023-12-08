ab9783895e44c84f6f76ea2a49f87673b611ddf6
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-12-08 13:41:42 +0530

    Test for Matrix{eltype} 1
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/pdmtypes.jl
+++ test/pdmtypes.jl
@@ -179,7 +179,7 @@ using Test
         @test M isa PDMat
         @test Matrix(M) ≈ A
         Mat32 = @inferred Matrix{Float32}(M)
-        @test eltype(Mat32) == Float32
+        @test Mat32 isa Matrix{Float32}
         @test Mat32 ≈ Float32.(A)
 
         M = @inferred AbstractPDMat(Diagonal(A))
