fb95b4a15ab4b72348905bc8301f94096a397bb8
Seth Axen <seth@sethaxen.com>
2025-05-09 13:12:50 +0200

    Test congruent transforms

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 include("testutils.jl")
-tests = ["pdmtypes", "abstracttypes", "addition", "generics", "kron", "chol", "specialarrays", "sqrt", "ad"]
+tests = ["pdmtypes", "abstracttypes", "addition", "congruent", "generics", "kron", "chol", "specialarrays", "sqrt", "ad"]
 println("Running tests ...")
 
 for t in tests
