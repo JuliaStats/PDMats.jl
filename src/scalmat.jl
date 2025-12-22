9fb4116ea84ce16ea6b763d08740377e75868734
Tim Holy <tim.holy@gmail.com>
2025-12-22 06:56:26 -0600

    Specialize for types that don't cache cholesky

--- src/scalmat.jl
+++ src/scalmat.jl
@@ -67,6 +67,11 @@ LinearAlgebra.eigmax(a::ScalMat) = a.value
 LinearAlgebra.eigmin(a::ScalMat) = a.value
 LinearAlgebra.sqrt(a::ScalMat) = ScalMat(a.dim, sqrt(a.value))
 
+function LinearAlgebra.ldiv!(A::ScalMat, B::AbstractVecOrMat)
+    @check_argdims A.dim == size(B, 1)
+    return rdiv!(B, A.value)
+end
+
 
 ### whiten and unwhiten
 
